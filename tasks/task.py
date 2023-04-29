3ad862cbeff4cbb40a628ba393fbf2e6dc5a185a
BugZ-EKB <e.shorohov87@yandex.ru>
2023-04-29 20:46:37 +0400

    Solution 1

--- tasks/task.py
+++ tasks/task.py
@@ -3,9 +3,21 @@ from typing import Dict, Any, Callable, Iterable
 DataType = Iterable[Dict[str, Any]]
 ModifierFunc = Callable[[DataType], DataType]
 
-
 def query(data: DataType, selector: ModifierFunc,
           *filters: ModifierFunc) -> DataType:
+    a = selector
+    for filter in filters:
+        data = filter(data)
+    result=[]
+    for dicts in data:
+        result_row = {}
+        for k in dicts.keys():
+            if k in a:
+                result_row[k] = dicts[k]
+        result.append(result_row)
+    return result
+
+    """Return function that selects only specific columns from dataset"""
     """
     Query data with column selection and filters
 
@@ -14,22 +26,24 @@ def query(data: DataType, selector: ModifierFunc,
     :param filters: Any number of results of `field_filter` function calls
     :return: Filtered data
     """
-    pass
-
 
 def select(*columns: str) -> ModifierFunc:
+    return list(columns)
     """Return function that selects only specific columns from dataset"""
-    pass
 
 
 def field_filter(column: str, *values: Any) -> ModifierFunc:
+    def filter_func(data):
+        return [row for row in data if row[column] in values]
+    return filter_func
     """Return function that filters specific column to be one of `values`"""
     pass
 
 
 def test_query():
     friends = [
-        {'name': 'Sam', 'gender': 'male', 'sport': 'Basketball'}
+        {'name': 'Sam', 'gender': 'male', 'sport': 'Basketball'},
+        {'name': 'Emily', 'gender': 'female', 'sport': 'volleyball'},
     ]
     value = query(
         friends,
@@ -42,4 +56,3 @@ def test_query():
 
 if __name__ == "__main__":
     test_query()
-
