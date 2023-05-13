00f4028c80e03f33c85e3bf744222ad16bf9e9e0
BugZ-EKB <e.shorohov87@yandex.ru>
2023-05-13 20:18:02 +0400

    Solution 3

--- tasks/task.py
+++ tasks/task.py
@@ -3,21 +3,33 @@ from typing import Dict, Any, Callable, Iterable
 DataType = Iterable[Dict[str, Any]]
 ModifierFunc = Callable[[DataType], DataType]
 
+
 def query(data: DataType, selector: ModifierFunc,
           *filters: ModifierFunc) -> DataType:
     a = selector
-    for filter in filters:
-        data = filter(data)
-    result=[]
-    for dicts in data:
-        result_row = {}
-        for k in dicts.keys():
+    result = []
+    for row in data:
+        b = {}
+        for k in row:
             if k in a:
-                result_row[k] = dicts[k]
-        result.append(result_row)
+                b[k] = row[k]
+        result.append(b)
+    # print(result)
+
+    for filter in filters:
+        filter_value = []
+        # print(filter)
+        for i in filter.keys():
+            f_key = str(i)
+            for j in filter[i]:
+                filter_value.append(j)
+        # print(f_key, filter_value)
+        for row in result:
+            if row[f_key] not in filter_value:
+                result.remove(row)
+
     return result
 
-    """Return function that selects only specific columns from dataset"""
     """
     Query data with column selection and filters
 
@@ -26,30 +38,40 @@ def query(data: DataType, selector: ModifierFunc,
     :param filters: Any number of results of `field_filter` function calls
     :return: Filtered data
     """
-
-def select(*columns: str) -> ModifierFunc:
-    return list(columns)
-    """Return function that selects only specific columns from dataset"""
-
-
-def field_filter(column: str, *values: Any) -> ModifierFunc:
-    def filter_func(data):
-        return [row for row in data if row[column] in values]
-    return filter_func
-    """Return function that filters specific column to be one of `values`"""
     pass
 
+def select(*columns: str) -> ModifierFunc:
+    """Return function that selects only specific columns from dataset"""
+    selected_data = list(columns)
+    return selected_data
 
+def field_filter(column: str, *values: Any) -> ModifierFunc:
+    """Return function that filters specific column to be one of `values`"""
+    filter={}
+    filter[column]=values
+    return filter
+
+# friends = [
+#     {'name': 'Sam', 'gender': 'male', 'sport': 'Basketball'},
+#     {'name': 'Emily', 'gender': 'female', 'sport': 'Volleyball'},
+# ]
+#
+# result = query(
+#     friends,
+#     select('name', 'gender', 'sport'),
+#     field_filter('name', *('Emily', 'John')),
+#     field_filter('sport', *('Basketball', 'Volleyball')),
+# )
+# print(result)
 # def test_query():
 #     friends = [
-#         {'name': 'Sam', 'gender': 'male', 'sport': 'Basketball'},
-#         {'name': 'Emily', 'gender': 'female', 'sport': 'volleyball'},
+#         {'name': 'Sam', 'gender': 'male', 'sport': 'Basketball'}
 #     ]
 #     value = query(
 #         friends,
 #         select(*('name', 'gender', 'sport')),
-#         field_filter('sport', 'Basketball', 'volleyball'),
-#         field_filter('gender', 'male',),
+#         field_filter(*('sport', *('Basketball', 'volleyball'))),
+#         field_filter(*('gender', *('male',))),
 #     )
 #     assert [{'gender': 'male', 'name': 'Sam', 'sport': 'Basketball'}] == value
 #
